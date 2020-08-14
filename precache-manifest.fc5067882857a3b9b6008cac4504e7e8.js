ca0724fc302f53839b77610297a95a538bdceaf7
theericwright <theericwright@aol.com>
2020-08-13 20:37:03 -0400

    Updates

---
+++ precache-manifest.fc5067882857a3b9b6008cac4504e7e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f813c8bd743d68bfb849fd37b0dc7fa",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "4bc462580e719ba473a5",
+    "url": "/ToDoApp/static/css/main.8edc3397.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/ToDoApp/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc462580e719ba473a5",
+    "url": "/ToDoApp/static/js/main.a6caaa3b.chunk.js"
+  },
+  {
+    "revision": "19767ff3f47648046f59",
+    "url": "/ToDoApp/static/js/runtime-main.0716006f.js"
+  }
+]);
